ce414fb4448f181ba904de99dd2bcfe80dd52106
williamhedenskog <94441813+pilsnerfrajz@users.noreply.github.com>
2023-11-28 10:28:32 +0100

    Add cards to details view

--- starwarswiki/src/views/detailsView.jsx
+++ starwarswiki/src/views/detailsView.jsx
@@ -1,3 +1,5 @@
+import Card from '../components/Card';
+
 function DetailsView(props) {
 	return (
 		<div>
@@ -5,8 +7,14 @@ function DetailsView(props) {
 			<img src={props.image}></img>
 			<h3>Description</h3>
 			<div>{props.details}</div>
+			<h3>You may also like</h3>
+			{props.suggested.map(getCardCB)}
 		</div>
 	);
+
+	function getCardCB(character) {
+		return <Card id={character.id} image={character.image} name={character.name} />;
+	}
 }
 
 export default DetailsView;
