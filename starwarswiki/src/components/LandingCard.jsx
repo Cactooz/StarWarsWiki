7d6274d0265188375d0333d72df63808604a6bbb
Pontus Berglund <60647163+pontaberglund@users.noreply.github.com>
2023-11-28 16:09:32 +0100

    Landing page currently works but not responsive

--- starwarswiki/src/components/LandingCard.jsx
+++ starwarswiki/src/components/LandingCard.jsx
@@ -1,9 +1,14 @@
+import { Link } from 'react-router-dom';
 import '/src/style.css';
 
 export default function LandingCard(props) {
 	return (
-		<div className={`landing-card ${props.class}`} onClick={props.onClickACB}>
-			<p>{props.text}</p>
+		<div className='router-link'>
+			<Link to={props.linkTo} className='no-text-decoration'>
+				<div className={`landing-card ${props.class} noDecoration`}>
+					<p>{props.text}</p>
+				</div>
+			</Link>
 		</div>
 	);
 }
--- starwarswiki/src/views/landingPageView.jsx
+++ starwarswiki/src/views/landingPageView.jsx
@@ -1,19 +1,20 @@
 import '/src/style.css';
 
 import LandingCard from '../components/LandingCard';
+import { Link } from 'react-router-dom';
 
-export default function LandingPageView() {
+export default function LandingPageView(props) {
 	function onClickCardACB(event) {
 		//Fire custom event to change route
-		console.log(event);
+		console.log(event.target.children[0].innerText);
 	}
 
 	return (
 		<div>
 			<h1>STAR WARS WIKI</h1>
-			<LandingCard text='Characters' class='characters-landing-card' onClickACB={onClickCardACB} />
-			<LandingCard text='Vehicles' class='vehicles-landing-card' onClickACB={onClickCardACB} />
-			<LandingCard text='Planets' class='planets-landing-card' onClickACB={onClickCardACB} />
+			<LandingCard text='Characters' class='characters-landing-card' linkTo='/characters' />
+			<LandingCard text='Vehicles' class='vehicles-landing-card' linkTo='/vehicles' />
+			<LandingCard text='Planets' class='planets-landing-card' linkTo='/planets' />
 		</div>
 	);
 }
