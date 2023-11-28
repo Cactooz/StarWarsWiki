7581bf25416b1eb9001b17b5f361ebeee6889a74
Hugo <54435874+Cactooz@users.noreply.github.com>
2023-11-28 15:30:00 +0100

    Create fetch.js

---
+++ starwarswiki/src/fetch.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+import { useQuery } from 'react-query';
+
+function fetchApi(url, path, params, key) {
+	const { isLoading, isError, data, error } = useQuery({
+		queryKey: [key],
+		queryFn: () =>
+			axios.get(`${url}${path}?${new URLSearchParams(params)}`).then((res) => res.data),
+	});
+
+	return { loading: isLoading, error: isError, data: data };
+}
+
+export function fetchSWAPI(path, params) {
+	return fetchApi('https://swapi.dev/api/', path, params, 'SWAPI');
+}
+
+export function fetchSWDatabank(path, params) {
+	return fetchApi(
+		'https://starwars-databank-server.vercel.app/api/v1/',
+		path,
+		params,
+		'SWDatabase',
+	);
+}
