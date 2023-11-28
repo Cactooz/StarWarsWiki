26160b86e1e39870583801a951dd5a028f3e631d
williamhedenskog <94441813+pilsnerfrajz@users.noreply.github.com>
2023-11-28 19:24:04 +0100

    Add fetch dependent on URL

--- starwarswiki/src/presenters/detailsPresenter.jsx
+++ starwarswiki/src/presenters/detailsPresenter.jsx
@@ -1,5 +1,25 @@
 import { observer } from 'mobx-react-lite';
 import DetailsView from '../views/detailsView';
+import { fetchSWDatabank } from '../fetch';
+
+const related = [
+	{
+		id: '64292927021f17e13fbc1efd',
+		name: 'Cliegg Lars',
+		image: 'https://lumiere-a.akamaihd.net/v1/images/databank_cliegglars_01_169_c2f0b9cb.jpeg',
+	},
+	{
+		id: '64292927021f17e13fbc1f01',
+		name: 'Clone Captain Howzer',
+		image: 'https://lumiere-a.akamaihd.net/v1/images/clone-captain-howzer-main_149c5805.jpeg',
+	},
+	{
+		id: '64292927021f17e13fbc1f09',
+		name: 'Clone Commander Jet',
+		image:
+			'https://lumiere-a.akamaihd.net/v1/images/databank_clonecommanderjet_01_169_e88dd6fb.jpeg',
+	},
+];
 
 export default observer(function Details(props) {
 	function showDetailsACB(id) {}
@@ -11,17 +31,23 @@ export default observer(function Details(props) {
 		return newPath[newPath.length - 1];
 	}
 
-	page = parseURL(path);
+	let page = 'characters/name/' + parseURL(path);
+	const recievedData = fetchSWDatabank(page, {});
 
-	// fetch data with page as name
+	if (recievedData.loading && !recievedData.error) return 'Loading...';
+	else if (!recievedData.loading && !recievedData.error) {
+		const name = recievedData.data[0].name;
+		const desc = recievedData.data[0].description;
+		const image = recievedData.data[0].image;
 
-	return (
-		<DetailsView
-			details={details}
-			image={image}
-			name={name}
-			suggested={related}
-			showDetails={showDetailsACB}
-		></DetailsView>
-	);
+		return (
+			<DetailsView
+				details={desc}
+				image={image}
+				name={name}
+				suggested={related}
+				showDetails={showDetailsACB}
+			></DetailsView>
+		);
+	}
 });
