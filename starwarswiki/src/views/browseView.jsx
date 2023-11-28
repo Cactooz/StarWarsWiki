6cb5caae9845f803091428b1f7ec912877d816b1
Rikard Löwbeer <lowbeer@kth.se>
2023-11-28 09:27:52 +0100

    Update browseView.jsx

--- starwarswiki/src/views/browseView.jsx
+++ starwarswiki/src/views/browseView.jsx
@@ -12,6 +12,7 @@ function showAllCB(card){
     return(
     <div key = {card.id} className="cards" onClick={showDetailsACB}>
         <img src={card.image} height={'100'}></img>
+        <div>{card.name}</div>
     </div>
     );
 }
