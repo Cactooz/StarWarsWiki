9d4fdddf9e66be16a8a1746fb58b4e5bfd1f8dcc
williamhedenskog <94441813+pilsnerfrajz@users.noreply.github.com>
2023-11-28 19:40:03 +0100

    Remove swapiInfo

--- starwarswiki/src/views/detailsView.jsx
+++ starwarswiki/src/views/detailsView.jsx
@@ -14,43 +14,16 @@ function DetailsView(props) {
 			<p>{props.details}</p>
 			<h3>You may also like</h3>
 			{props.suggested.map(getCardCB)}
-			{swapiInfo(props.swapiData)}
 		</div>
 	);
-
-	function swapiInfo(data) {
-		return (
-			<div>
-				<span>Birth year: {data.birth_year}</span>
-				<span>Gender: {data.gender}</span>
-				<span>Species: {data.species}</span>
-				<span>Homeworld: {data.homeworld}</span>
-				<h3>Starships</h3>
-				{data.starships.map(starshipACB)}
-			</div>
-		);
-	}
-
-	function starshipACB(ships) {
-		return (
-			<div>
-				<img src={ships} />
-			</div>
-		);
-	}
 
 	function showDetailsACB(id) {
 		props.showDetails(id);
 	}
 
-	function getCardCB(character) {
+	function getCardCB(object) {
 		return (
-			<Card
-				key={character.id}
-				image={character.image}
-				name={character.name}
-				onClick={showDetailsACB}
-			/>
+			<Card key={object.id} image={object.image} name={object.name} onClick={showDetailsACB} />
 		);
 	}
 }
