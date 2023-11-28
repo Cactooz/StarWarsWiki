16b2a363fb54cf303820c325bffa3813346eb6dd
williamhedenskog <94441813+pilsnerfrajz@users.noreply.github.com>
2023-11-28 16:56:04 +0100

    Start parsing URL to fetch

--- starwarswiki/src/presenters/detailsPresenter.jsx
+++ starwarswiki/src/presenters/detailsPresenter.jsx
@@ -4,6 +4,17 @@ import DetailsView from '../views/detailsView';
 export default observer(function Details(props) {
 	function showDetailsACB(id) {}
 
+	let path = window.location.pathname;
+
+	function parseURL(path) {
+		const newPath = path.split('/');
+		return newPath[newPath.length - 1];
+	}
+
+	page = parseURL(path);
+
+	// fetch data with page as name
+
 	return (
 		<DetailsView
 			details={details}
