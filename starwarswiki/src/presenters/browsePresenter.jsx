e0b839a1362ecf64e0a08122a68a4247518652ab
Rikard Löwbeer <lowbeer@kth.se>
2023-11-29 10:23:32 +0100

    Update browsePresenter.jsx

--- starwarswiki/src/presenters/browsePresenter.jsx
+++ starwarswiki/src/presenters/browsePresenter.jsx
@@ -2,19 +2,20 @@ import {observer} from "mobx-react-lite";
 import BrowseView from "../views/browseView.jsx";
 import Vortex from "../components/Vortex.jsx";
 export default
-observer(
     function Browse(props){
-        function render(data){
-            if(!data)
+        function render(data) {
+            if (!data) {
                 return "no data";
-            else if(data.loading)
-                return <Vortex/>;
-            else if(data.error)
-                return data.error;
+            }
+            else if (data.loading) {
+            return <Vortex/>;
+            }
+            else if(data.error) {
+                return data.error
+            }
             else
                 return <BrowseView browseResult={data.data.data}/>
         }
 
     return render(props.model.searchResult)
 }
-);
