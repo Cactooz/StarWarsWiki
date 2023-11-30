8dbde738edff3d40c2a4480371a97d475292720e
Hugo <54435874+Cactooz@users.noreply.github.com>
2023-11-30 10:11:40 +0100

    Add fetch function for SWAPI & SWD (#11)
    * Add react-query
    
    * Add QueryClientProvider around app in main.jsx
    
    * Add axios package
    
    * Update settings.json
    
    * Create fetch.js
    
    * Catch soft errors to return error for SWDatabank
    
    * Add refresh and query limit options
    
    * Remove React.StrictMode to fix double fetch

---
+++ starwarswiki/src/fetch.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+import { useQuery } from 'react-query';
+
+function fetchApi(url, path, params, key) {
+	const { isLoading, isError, data } = useQuery({
+		concurrentQueriesLimit: 5,
+		staleTime: 86400000,
+		queryKey: [key],
+		queryFn: () =>
+			axios.get(`${url}${path}?${new URLSearchParams(params)}`).then((res) => res.data),
+	});
+
+	return { loading: isLoading, error: isError, data: data };
+}
+
+export function fetchSWAPI(path, params) {
+	return fetchApi('https://swapi.dev/api/', path, params, 'SWAPI');
+}
+
+export function fetchSWDatabank(path, params) {
+	let { loading, error, data } = fetchApi(
+		'https://starwars-databank-server.vercel.app/api/v1/',
+		path,
+		params,
+		'SWDatabase',
+	);
+
+	if (data === "These aren't the droids you're looking for...") error = true;
+
+	return { loading: loading, error: error, data: data };
+}
