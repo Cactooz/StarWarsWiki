83ac8298a207d6c52727940b9add765806538acf
williamhedenskog <94441813+pilsnerfrajz@users.noreply.github.com>
2023-12-06 13:03:42 +0100

    Use data from model

--- starwarswiki/src/presenters/detailsPresenter.jsx
+++ starwarswiki/src/presenters/detailsPresenter.jsx
@@ -4,15 +4,22 @@ import { getDetails } from '../utilities';
 import Vortex from '../components/Vortex.jsx';
 
 export default observer(function Details(props) {
-	const recievedData = getDetails();
+	let path = window.location.pathname;
+	const splitURL = path.split('/');
+	let page = splitURL[splitURL.length - 2] + '/name/' + splitURL[splitURL.length - 1];
 
-	if (recievedData.loading && !recievedData.error) return <Vortex />;
-	else if (recievedData.error) return 'Error';
-	else if (!recievedData.loading && !recievedData.error) {
-		const name = recievedData.data[0].name;
-		const desc = recievedData.data[0].description;
-		const image = recievedData.data[0].image;
-
-		return <DetailsView details={desc} image={image} name={name}></DetailsView>;
+	console.log(page);
+	if (!props.model.details || props.model.currentDetails !== page) {
+		props.model.setDetails(page);
+		return <Vortex />;
+	} else if (props.model.details === null) return 'Error';
+	else {
+		return (
+			<DetailsView
+				details={props.model.details[0].description}
+				image={props.model.details[0].image}
+				name={props.model.details[0].name}
+			></DetailsView>
+		);
 	}
 });
