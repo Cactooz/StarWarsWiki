bf1d1834d8670260b1680a4abad8b16e3ee98dde
Pontus Berglund <60647163+pontaberglund@users.noreply.github.com>
2023-12-06 15:01:02 +0100

    Fire custom events

--- starwarswiki/src/presenters/headerPresenter.jsx
+++ starwarswiki/src/presenters/headerPresenter.jsx
@@ -1,5 +1,5 @@
 import HeaderView from '../views/headerView';
 
-export default function HeaderPresenter() {
-	return <HeaderView />;
+export default function HeaderPresenter(props) {
+	return <HeaderView onClickHanlder={props.model.setBrowseResult} />;
 }
--- starwarswiki/src/views/headerView.jsx
+++ starwarswiki/src/views/headerView.jsx
@@ -1,18 +1,22 @@
 import { Link } from 'react-router-dom';
 
-export default function HeaderView() {
+export default function HeaderView(props) {
+	function browseDataACB(event) {
+		props.onClickHandler(event.target.innerText.toLowerCase());
+	}
+
 	return (
 		<header className='header'>
 			<Link to='/'>
 				<p>Home</p>
 			</Link>
-			<Link to='/characters'>
+			<Link to='/characters' onClick={browseDataACB}>
 				<p>Characters</p>
 			</Link>
-			<Link to='/vehicles'>
+			<Link to='/vehicles' onClick={browseDataACB}>
 				<p>Vehicles</p>
 			</Link>
-			<Link to='/locations'>
+			<Link to='/locations' onClick={browseDataACB}>
 				<p>Locations</p>
 			</Link>
 		</header>
