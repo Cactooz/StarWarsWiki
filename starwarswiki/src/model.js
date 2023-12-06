56b15f04931a73c86727563e09e2b778efd3405e
Rikard Löwbeer <lowbeer@kth.se>
2023-12-06 17:20:11 +0100

    Add user property in model

--- starwarswiki/src/model.js
+++ starwarswiki/src/model.js
@@ -2,11 +2,16 @@ import {fetchSWDatabank} from './fetch.js';
 import {queryClient} from './main.jsx';
 
 export default {
+	user: undefined,
 	currentBrowse: undefined,
 	browseResult: {},
 
 	currentDetails: undefined,
 	details: {},
+
+	setUser(id) {
+		this.user = id;
+	},
 
 	async setDetails(params) {
 		await fetchSWDatabank(params, {}, params);
