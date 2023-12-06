c5a0f097bc2ed98a8ba1cb4cc945b1f80d335d66
Rikard Löwbeer <lowbeer@kth.se>
2023-12-06 11:53:43 +0100

    format

--- starwarswiki/src/model.js
+++ starwarswiki/src/model.js
@@ -1,21 +1,17 @@
 import {fetchSWDatabank} from "./fetch.js";
 import {queryClient} from "./main.jsx";
-import axios from "axios";
 
 export default {
-	currentBrowse: undefined,
-	browseResult: {},
+    currentBrowse: undefined,
+    browseResult: {},
 
+    async setBrowseResult(params) {
+        await fetchSWDatabank(params, {}, params)
+        this.browseResult = queryClient.getQueryData(params);
+        this.currentBrowse = params;
+    }
 
-
-	async setBrowseResult(params) {
-		await fetchSWDatabank(params,{}, params)
-		this.browseResult = queryClient.getQueryData(params);
-		this.currentBrowse = params;
-
-	}
-
-	/*addFavorite(path, id) {},
-	Firebase stuff
-	removeFavorite(path, id) {},*/
+    /*addFavorite(path, id) {},
+    Firebase stuff
+    removeFavorite(path, id) {},*/
 };
--- starwarswiki/src/presenters/browsePresenter.jsx
+++ starwarswiki/src/presenters/browsePresenter.jsx
@@ -1,21 +1,20 @@
 import BrowseView from '../views/browseView.jsx';
 import Vortex from '../components/Vortex.jsx';
 import {observer} from 'mobx-react-lite';
-import {fetchSWDatabank} from "../fetch.js";
 
 
 export default observer(function Browse(props) {
-	function render(browseResult) {
-		const site = window.location.pathname.replace("/", "");
-		if(props.model.currentBrowse === undefined || props.model.currentBrowse !== site) {
-			props.model.setBrowseResult(site);
-			return <Vortex/>;
-		}
-		else if(browseResult === null)
-			return <div>Error While Loading. Please Try Again!</div>
-		else {
-			return <BrowseView browseResult={browseResult.data}/>;
-		}
-	}
-	return render(props.model.browseResult);
+    function render(browseResult) {
+        const site = window.location.pathname.replace("/", "");
+        if (props.model.currentBrowse === undefined || props.model.currentBrowse !== site) {
+            props.model.setBrowseResult(site);
+            return <Vortex/>;
+        } else if (browseResult === null)
+            return <div>Error While Loading. Please Try Again!</div>
+        else {
+            return <BrowseView browseResult={browseResult.data}/>;
+        }
+    }
+
+    return render(props.model.browseResult);
 });
