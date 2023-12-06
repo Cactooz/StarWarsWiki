020a7434f5405018c14b93366ec9b70c629d9acc
Rikard Löwbeer <lowbeer@kth.se>
2023-12-06 20:16:18 +0100

    addEventListener with custom function

--- starwarswiki/src/presenters/browsePresenter.jsx
+++ starwarswiki/src/presenters/browsePresenter.jsx
@@ -4,17 +4,22 @@ import {observer} from 'mobx-react-lite';
 
 
 export default observer(function Browse(props) {
-    function render(browseResult) {
-        const site = window.location.pathname.replace("/", "");
-        if (props.model.currentBrowse === undefined || props.model.currentBrowse !== site) {
-            props.model.setBrowseResult(site);
-            return <Vortex/>;
-        } else if (browseResult === null)
-            return <div>Error While Loading. Please Try Again!</div>
-        else {
-            return <BrowseView browseResult={browseResult.data}/>;
-        }
-    }
 
-    return render(props.model.browseResult);
+	function reset() {
+		props.model.unSetCurrentBrowse();
+	}
+
+	function render(browseResult) {
+		const site = window.location.pathname.replace("/", "");
+		if (props.model.currentBrowse === undefined || props.model.currentBrowse !== site) {
+			props.model.setBrowseResult(site);
+			return <Vortex/>;
+		} else if (browseResult === null)
+			return <div>Error While Loading. Please Try Again!</div>
+		else {
+			return <BrowseView browseResult={browseResult.data} resetPage={reset}/>;
+		}
+	}
+
+	return render(props.model.browseResult);
 });
--- starwarswiki/src/views/browseView.jsx
+++ starwarswiki/src/views/browseView.jsx
@@ -1,18 +1,22 @@
 import Card from "../components/Card.jsx";
-export default function browseView(props){
-return(
-    <div>
-        {props.browseResult.map(showAllCB)}
-    </div>
-);
-function showAllCB(card){
-    function showDetailsACB(event){
-        console.log(event.target)
-        //FIRE CUSTOM EVENT HERE AND USE ROUTER
-    }
-    return(
-    <Card key={card._id} name ={card.name} image={card.image} onClick={showDetailsACB}/>
-    );
-}
+
+export default function browseView(props) {
+	function reset() {
+		props.resetPage()
+	}
+
+	addEventListener("popstate", reset)
+
+	return (
+		<div>
+			{props.browseResult.map(showAllCB)}
+		</div>
+	);
+
+	function showAllCB(card) {
+		return (
+			<Card key={card._id} name={card.name} image={card.image}/>
+		);
+	}
 }
 
