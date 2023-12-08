20e30b4df5261e362671fbedf68a462b2999d60f
Rikard Löwbeer <118805329+LowbeerR@users.noreply.github.com>
2023-12-08 09:17:43 +0100

    Reset data on back button browse (#31)
    * addEventListener with custom function
    
    * Update Card.jsx
    
    remove unused onclick
    
    * Update browseView.jsx
    
    change listener
    
    * Revert "Update browseView.jsx"
    
    This reverts commit 20381102a9bf7db1a2471da2aa965944958967bf.
    
    * New try
    
    Using useLocation() to see if site has changed.

--- starwarswiki/src/components/Card.jsx
+++ starwarswiki/src/components/Card.jsx
@@ -1,14 +1,14 @@
 import {Link} from "react-router-dom";
 import "../style.css"
 
-export default function(card) {
-return (
-    <div className="cards" onClick={card.onClick}>
-        <Link to={card.name}>
-        <img src={card.image} height={'100'}></img>
-        <div>{card.name}</div>
-        </Link>
-    </div>
+export default function (card) {
+	return (
+		<div className="cards">
+			<Link to={card.name}>
+				<img src={card.image} height={'100'}></img>
+				<div>{card.name}</div>
+			</Link>
+		</div>
 
-);
+	);
 }
--- starwarswiki/src/views/browseView.jsx
+++ starwarswiki/src/views/browseView.jsx
@@ -1,18 +1,17 @@
 import Card from "../components/Card.jsx";
-export default function browseView(props){
-return(
-    <div>
-        {props.browseResult.map(showAllCB)}
-    </div>
-);
-function showAllCB(card){
-    function showDetailsACB(event){
-        console.log(event.target)
-        //FIRE CUSTOM EVENT HERE AND USE ROUTER
-    }
-    return(
-    <Card key={card._id} name ={card.name} image={card.image} onClick={showDetailsACB}/>
-    );
-}
+
+export default function browseView(props) {
+
+	return (
+		<div>
+			{props.browseResult.map(showAllCB)}
+		</div>
+	);
+
+	function showAllCB(card) {
+		return (
+			<Card key={card._id} name={card.name} image={card.image}/>
+		);
+	}
 }
 
