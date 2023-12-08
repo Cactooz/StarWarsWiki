08741d751b9cec87cb00b94f01ef960729538ab9
Rikard Löwbeer <118805329+LowbeerR@users.noreply.github.com>
2023-12-08 09:17:43 +0100

    Reset data on back button browse (#31)
    * addEventListener with custom function
    
    * Update Card.jsx
    
    remove unused onclick
    
    * Update browseView.jsx
    
    change listener
    
    * Revert "Update browseView.jsx"
    
    This reverts commit 20381102a9bf7db1a2471da2aa965944958967bf.
    
    * New try
    
    Using useLocation() to see if site has changed.

--- starwarswiki/src/presenters/browsePresenter.jsx
+++ starwarswiki/src/presenters/browsePresenter.jsx
@@ -1,20 +1,22 @@
 import BrowseView from '../views/browseView.jsx';
 import Vortex from '../components/Vortex.jsx';
 import {observer} from 'mobx-react-lite';
+import {useLocation} from "react-router-dom";
 
 
 export default observer(function Browse(props) {
-    function render(browseResult) {
-        const site = window.location.pathname.replace("/", "");
-        if (props.model.currentBrowse === undefined || props.model.currentBrowse !== site) {
-            props.model.setBrowseResult(site);
-            return <Vortex/>;
-        } else if (browseResult === null)
-            return <div>Error While Loading. Please Try Again!</div>
-        else {
-            return <BrowseView browseResult={browseResult.data}/>;
-        }
-    }
 
-    return render(props.model.browseResult);
+	function render(browseResult) {
+		const site = useLocation().pathname.replace("/", "");
+		if (props.model.currentBrowse === undefined || props.model.currentBrowse !== site) {
+			props.model.setBrowseResult(site);
+			return <Vortex/>;
+		} else if (browseResult === null)
+			return <div>Error While Loading. Please Try Again!</div>
+		else {
+			return <BrowseView browseResult={browseResult.data}/>;
+		}
+	}
+
+	return render(props.model.browseResult);
 });
