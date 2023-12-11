e33475b4fb3b9e4161449007dc2bbcc283b76433
Rikard Löwbeer <lowbeer@kth.se>
2023-12-11 15:01:52 +0100

    update profile
    render browse view
    
    Co-Authored-By: William Hedenskog <94441813+pilsnerfrajz@users.noreply.github.com>

--- starwarswiki/src/views/profileView.jsx
+++ starwarswiki/src/views/profileView.jsx
@@ -1,15 +1,7 @@
 import '/src/style.css';
-import Card from "../components/Card.jsx";
 import SignOutButton from "../components/SignOutButton.jsx";
 
 export default function ProfileView(props) {
-
-	function showFavoritesCB(card) {
-		return (
-			<Card key={card._id} name={card.name} image={card.image}/>
-		);
-	}
-
 	return (
 		<>
 			<SignOutButton/>
@@ -17,9 +9,6 @@ export default function ProfileView(props) {
 			<img src={props.currentUser.photoURL}/>
 			<div>
 				<h3>These Are Your Favorites!</h3>
-				<div>
-					{!props.favorites.length ? "You haven't added any favorites yet..." : props.favorites.map(showFavoritesCB)}
-				</div>
 			</div>
 		</>
 	);
