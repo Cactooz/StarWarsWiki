3d82329a8b244d90d40b5f9856a6dbc560b044e7
Pontus <60647163+pontaberglund@users.noreply.github.com>
2023-12-11 20:22:17 +0100

    Search bar (#39)
    * Add 3rd party search bar component
    
    * Fetch and format all search options
    
    * Add all search options
    
    * Add searchBar presenter
    
    * Add searchBar to landing page
    
    * Add 3rd party search bar component
    
    * Fetch and format all search options
    
    * Add all search options
    
    * Add searchBar presenter
    
    * Add searchBar to landing page
    
    * Move api scraping to utils
    
    * Remove creatures and droids

---
+++ utils/convertToList.js
@@ -0,0 +1,19 @@
+import * as allData from './allData.json' assert { type: 'json' };
+import { writeFile } from 'fs';
+
+const numberOfCharacters = 964;
+const numberOfLocations = 326;
+const numberOfVehicles = 267;
+
+//console.log(allData);
+
+const data = allData.default.flat(Infinity).map(({ _id, name, description, image, _v }, index) => {
+	let type;
+	if (index < numberOfCharacters) type = 'characters';
+	else if (index < numberOfCharacters + numberOfLocations) type = 'locations';
+	else if (index < numberOfCharacters + numberOfLocations + numberOfVehicles) type = 'vehicles';
+	return { id: _id, name: name, type: type };
+});
+
+let json = JSON.stringify(data);
+writeFile('autoCompleteList.json', json, 'utf-8', () => {});
