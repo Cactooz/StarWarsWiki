d85aa81467cbc40b35bd0793493072ccce30955b
williamhedenskog <94441813+pilsnerfrajz@users.noreply.github.com>
2023-12-11 21:13:07 +0100

    Add fav buttons to detailsView

--- starwarswiki/src/views/detailsView.jsx
+++ starwarswiki/src/views/detailsView.jsx
@@ -1,8 +1,29 @@
 function DetailsView(props) {
+	function findFavCB(item) {
+		return item.name === props.name;
+	}
+
+	function removeACB() {
+		props.removeFavorite(object);
+	}
+
+	function addACB() {
+		props.addFavorite(object);
+	}
+
+	const object = { id: props.id, name: props.name, image: props.image, path: props.path };
+
 	return (
 		<div>
 			<h2>{props.name}</h2>
 			<img src={props.image}></img>
+			<div>
+				{props.fav.find(findFavCB) ? (
+					<button onClick={removeACB}>Remove Favorite</button>
+				) : (
+					<button onClick={addACB}>Add Favorite</button>
+				)}
+			</div>
 			<h3>Description</h3>
 			<p>{props.details}</p>
 		</div>
