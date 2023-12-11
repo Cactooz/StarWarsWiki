be4a0cebad661779d5062ed43167d02836f5a12c
Rikard Löwbeer <lowbeer@kth.se>
2023-12-11 13:46:32 +0100

    Update browse and card
    Update with add favorite button
    
    Co-Authored-By: William Hedenskog <94441813+pilsnerfrajz@users.noreply.github.com>

--- starwarswiki/src/views/browseView.jsx
+++ starwarswiki/src/views/browseView.jsx
@@ -2,6 +2,14 @@ import Card from "../components/Card.jsx";
 
 export default function browseView(props) {
 
+	function removeACB(card) {
+		props.doRemove(card);
+	}
+
+	function addACB(card) {
+		props.doAdd(card);
+	}
+
 	return (
 		<div>
 			{props.browseResult.map(showAllCB)}
@@ -10,7 +18,8 @@ export default function browseView(props) {
 
 	function showAllCB(card) {
 		return (
-			<Card key={card._id} name={card.name} image={card.image}/>
+			<Card key={card._id} name={card.name} image={card.image} removeFavorite={removeACB} addFavorite={addACB}
+			      fav={props.fav}/>
 		);
 	}
 }
