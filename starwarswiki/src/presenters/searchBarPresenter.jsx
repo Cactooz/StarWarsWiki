a22f601a8e2e6647bd5f33d495e0b640d4be901f
Pontus Berglund <60647163+pontaberglund@users.noreply.github.com>
2023-12-12 11:03:06 +0100

    Add a handleFormSelect

--- starwarswiki/src/presenters/searchBarPresenter.jsx
+++ starwarswiki/src/presenters/searchBarPresenter.jsx
@@ -1,13 +1,37 @@
 import items from '../data/autoCompleteList.json';
 import { ReactSearchAutocomplete } from 'react-search-autocomplete';
 import { useNavigate } from 'react-router-dom';
+import SearchBarView from '../views/searchBarView';
+import { useState } from 'react';
 
 export default function SearchBarPresenter(props) {
 	const navigate = useNavigate();
+	const [results, setResults] = useState([]);
 
-	function handleOnSelect(item) {
-		navigate(item.type + '/' + item.name);
+	function handleSearchSelect(item) {
+		let name = item.name.replaceAll('/', '%2F').replaceAll('.', '%2E');
+		navigate(item.type + '/' + name);
 	}
 
-	return <ReactSearchAutocomplete items={items} onSelect={handleOnSelect} />;
+	function handleFormSubmit(event) {
+		event.preventDefault();
+		if (results.length !== 0) {
+			let item = results[0];
+			let name = item.name.replaceAll('/', '%2F').replaceAll('.', '%2E');
+			navigate(item.type + '/' + name);
+		}
+	}
+
+	function handleOnSearch(string, results) {
+		setResults(results);
+	}
+
+	return (
+		<SearchBarView
+			handleSearchSelect={handleSearchSelect}
+			handleFormSubmit={handleFormSubmit}
+			handleOnSearch={handleOnSearch}
+			items={items}
+		/>
+	);
 }
--- starwarswiki/src/views/searchBarView.jsx
+++ starwarswiki/src/views/searchBarView.jsx
@@ -2,7 +2,7 @@ import { ReactSearchAutocomplete } from 'react-search-autocomplete';
 
 export default function SearchBarView(props) {
 	return (
-		<form onSubmit={props.handleFormSelect}>
+		<form onSubmit={props.handleFormSubmit}>
 			<ReactSearchAutocomplete
 				items={props.items}
 				onSelect={props.handleSearchSelect}
