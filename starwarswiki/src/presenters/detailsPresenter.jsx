59f0f2f826dc2eea31d273af33c72b46f3910cfb
Hugo <54435874+Cactooz@users.noreply.github.com>
2023-12-12 11:49:19 +0100

    Add moreDetailView to detailsPresenter

--- starwarswiki/src/presenters/detailsPresenter.jsx
+++ starwarswiki/src/presenters/detailsPresenter.jsx
@@ -1,6 +1,15 @@
 import { observer } from 'mobx-react-lite';
 import DetailsView from '../views/detailsView';
+import MoreDetailsView from '../views/moreDetailsView';
 import Vortex from '../components/Vortex.jsx';
+
+const data = {
+	swapiId: 'cGVvcGxlOjE=',
+	swapiName: 'Luke Skywalker',
+	swapiType: 'People',
+	swdName: 'Luke Skywalker',
+};
+let moreDetails = false;
 
 export default observer(function Details(props) {
 	function addFavoriteACB(object) {
@@ -15,23 +24,53 @@ export default observer(function Details(props) {
 	const splitURL = path.split('/');
 	let page = splitURL[splitURL.length - 2] + '/name/' + splitURL[splitURL.length - 1];
 
-	if (!props.model.details || props.model.currentDetails !== page) {
+	let moreDetailsPath = atob(data.swapiId).split(':');
+	let moreDetailsPage = `${moreDetailsPath[0]}/${moreDetailsPath[1]}`;
+
+	if (!props.model.details || props.model.currentDetails !== page || !props.model.moreDetails) {
 		props.model.setDetails(page);
+		props.model.setMoreDetails(moreDetailsPage);
 		return <Vortex />;
-	} else if (props.model.details === null) return 'Error';
+	} else if (props.model.details === null || props.model.moreDetails === null) return 'Error';
 	else {
+		moreDetails = props.model.moreDetails[0]?.[1];
+
+		if (moreDetails) {
+			moreDetails = Object.keys(moreDetails).map((key) => {
+				return {
+					key: key,
+					value: moreDetails[key],
+				};
+			});
+
+			moreDetails = moreDetails.filter(({ key, value }) => {
+				if (key === 'name' || key === 'created' || key === 'edited') return false;
+				if (typeof value === 'string' && value.startsWith('http')) return false;
+				if (Array.isArray(value)) return false;
+				return true;
+			});
+
+			moreDetails = moreDetails.map((item) => ({
+				key: item.key.replace('_', ' '),
+				value: item.value,
+			}));
+		}
+
 		return (
-			<DetailsView
-				loggedIn={props.model.user}
-				details={props.model.details[0].description}
-				image={props.model.details[0].image}
-				name={props.model.details[0].name}
-				id={props.model.details[0]._id}
-				path={splitURL[splitURL.length - 2]}
-				fav={props.model.favorites}
-				removeFavorite={removeFavoriteACB}
-				addFavorite={addFavoriteACB}
-			></DetailsView>
+			<>
+				<DetailsView
+					loggedIn={props.model.user}
+					details={props.model.details[0].description}
+					image={props.model.details[0].image}
+					name={props.model.details[0].name}
+					id={props.model.details[0]._id}
+					path={splitURL[splitURL.length - 2]}
+					fav={props.model.favorites}
+					removeFavorite={removeFavoriteACB}
+					addFavorite={addFavoriteACB}
+				/>
+				{moreDetails && <MoreDetailsView details={moreDetails} />}
+			</>
 		);
 	}
 });
