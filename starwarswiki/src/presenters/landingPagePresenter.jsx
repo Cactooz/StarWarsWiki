4a7909dc00ba3cd7d8734dfa0bc441427f0f2226
Pontus Berglund <60647163+pontaberglund@users.noreply.github.com>
2023-12-12 15:29:42 +0100

    Move presenter out of view to other presenter

--- starwarswiki/src/presenters/landingPagePresenter.jsx
+++ starwarswiki/src/presenters/landingPagePresenter.jsx
@@ -1,8 +1,14 @@
 //Use window.location = "/characters"
 
 import LandingPageView from '../views/landingPageView';
+import SearchBarPresenter from './searchBarPresenter';
 import { observer } from 'mobx-react-lite';
 
 export default observer(function LandingPagePresenter(props) {
-	return <LandingPageView user={props.model.user} model={props.model} />;
+	return (
+		<>
+			<SearchBarPresenter model={props.model} />{' '}
+			<LandingPageView user={props.model.user} model={props.model} />
+		</>
+	);
 });
