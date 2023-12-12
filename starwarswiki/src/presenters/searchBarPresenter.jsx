18b20afc7923e140b50e06a87c2ba301d185751f
Pontus Berglund <60647163+pontaberglund@users.noreply.github.com>
2023-12-12 15:26:27 +0100

    Search on enter

--- starwarswiki/src/presenters/searchBarPresenter.jsx
+++ starwarswiki/src/presenters/searchBarPresenter.jsx
@@ -1,5 +1,4 @@
 import items from '../data/autoCompleteList.json';
-import { ReactSearchAutocomplete } from 'react-search-autocomplete';
 import { useNavigate } from 'react-router-dom';
 import SearchBarView from '../views/searchBarView';
 import { useState } from 'react';
@@ -13,13 +12,10 @@ export default function SearchBarPresenter(props) {
 		navigate(item.type + '/' + name);
 	}
 
-	function handleFormSubmit(event) {
-		event.preventDefault();
-		if (results.length !== 0) {
-			let item = results[0];
-			let name = item.name.replaceAll('/', '%2F').replaceAll('.', '%2E');
-			navigate(item.type + '/' + name);
-		}
+	async function handleFormSubmit(event) {
+		event.preventDefault(); //Stop page from refreshing
+		navigate('/search');
+		await props.model.setSearchResults(results);
 	}
 
 	function handleOnSearch(string, results) {
