3434834549df8f671e3ed18859fc480679bdcc73
Hugo <54435874+Cactooz@users.noreply.github.com>
2023-12-13 09:19:06 +0100

    Create moreDetailsPresenter.jsx

---
+++ starwarswiki/src/presenters/moreDetailsPresenter.jsx
@@ -0,0 +1,59 @@
+import { observer } from 'mobx-react-lite';
+import MoreDetailsView from '../views/moreDetailsView';
+import { readHash } from '../models/firebaseModel.js';
+
+export default observer(function MoreDetails(props) {
+	const splitURL = window.location.pathname.split('/');
+	const page = splitURL[splitURL.length - 2] + '/name/' + splitURL[splitURL.length - 1];
+
+	if (props.model.currentHash !== splitURL[splitURL.length - 2]) {
+		readHash(splitURL[splitURL.length - 2]);
+	}
+
+	let moreDetailsPage;
+
+	if (props.model.details[0] && props.model.currentHash === splitURL[splitURL.length - 2]) {
+		const hashedItem = props.model.hash[props.model.details?.[0]?._id];
+
+		if (hashedItem) {
+			const moreDetailsPath = atob(hashedItem.swapiId).split(':');
+			moreDetailsPage = `${moreDetailsPath[0]}/${moreDetailsPath[1]}`;
+		}
+
+		if (props.model.currentMoreDetails !== moreDetailsPage) {
+			props.model.setMoreDetails(moreDetailsPage);
+		}
+	}
+
+	if (
+		props.model.moreDetails[0] !== undefined &&
+		props.model.currentMoreDetails === moreDetailsPage
+	) {
+		let moreDetails = props.model.moreDetails[0]?.[1];
+
+		if (moreDetails) {
+			moreDetails = Object.keys(moreDetails).map((key) => {
+				return {
+					key: key,
+					value: moreDetails[key],
+				};
+			});
+
+			moreDetails = moreDetails.filter(({ key, value }) => {
+				if (key === 'name' || key === 'created' || key === 'edited') return false;
+				if (typeof value === 'string' && value.startsWith('http')) return false;
+				if (Array.isArray(value)) return false;
+				return true;
+			});
+
+			moreDetails = moreDetails.map((item) => ({
+				key: item.key.replaceAll('_', ' '),
+				value: item.value,
+			}));
+		}
+
+		return <>{moreDetails && <MoreDetailsView details={moreDetails} />}</>;
+	} else {
+		return;
+	}
+});
