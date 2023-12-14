18a5bc50c24f1fbd340b17bc1a4f6fd6133d10fd
Pontus Berglund <60647163+pontaberglund@users.noreply.github.com>
2023-12-14 16:46:25 +0100

    Merge header and searchbar into one presenter

--- starwarswiki/src/presenters/landingPagePresenter.jsx
+++ starwarswiki/src/presenters/landingPagePresenter.jsx
@@ -1,11 +1,6 @@
 import LandingPageView from '../views/landingPageView';
-import SearchBarPresenter from './searchBarPresenter';
 import { observer } from 'mobx-react-lite';
 
 export default observer(function LandingPagePresenter(props) {
-	return (
-		<>
-			<LandingPageView user={props.model.user} model={props.model} />
-		</>
-	);
+	return <LandingPageView user={props.model.user} model={props.model} />;
 });
