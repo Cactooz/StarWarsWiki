d430e7e82bb0d70fc33fec93650dc59846c01a02
Rikard Löwbeer <lowbeer@kth.se>
2023-12-14 19:08:20 +0100

    Update friendSidebarView.jsx
    add native events for accepting / declining

--- starwarswiki/src/views/friendSidebarView.jsx
+++ starwarswiki/src/views/friendSidebarView.jsx
@@ -7,12 +7,7 @@ export default function FriendSidebarView(props) {
 				onClick={props.shouldShowId === false ? props.showID : props.hideID}>{props.shouldShowId === false ? "Click to show your ID" : "Click to hide your ID"}</button>
 			<p>{props.shouldShowId === true ? props.yourID : ""}</p>
 			<h3>{props.friendRequest.length ? "Your Friend Requests" : ""}</h3>
-			{props.friendRequest.length ?
-				<div>
-					{props.friendRequest.map(showFriends)}
-					<button>Accept</button>
-					<button>Decline</button>
-				</div> : ""}
+			{props.friendRequest.length ? props.friendRequest.map(showFriends) : ""}
 			<h3>These Are Your Friends!</h3>
 			<p>Add friends with friend id:
 				<input type={"text"} placeholder={"Enter Your Friends ID"} onKeyUp={props.addfriend}/>
@@ -25,11 +20,22 @@ export default function FriendSidebarView(props) {
 	);
 
 	function showFriends(friend) {
-		return (<div key={friend}>{friend}</div>)
+		function acceptFriend() {
+			props.acceptFriend(friend)
+		}
+
+		function declineFriend() {
+			props.declineFriend(friend)
+		}
+
+		return (<div key={friend}>
+			{friend}
+			<button onClick={acceptFriend}>Accept</button>
+			<button onClick={declineFriend}>Decline</button>
+		</div>)
 	}
 
 	function showAllCB(friend) {
-		console.log(friend)
 		return (
 			<div key={friend}>
 				<Link to={friend} key={friend}>
