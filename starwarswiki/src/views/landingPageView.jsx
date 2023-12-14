d7d8e7dd4499fb3830c86a0440cbd4d27dba36ee
Hugo <54435874+Cactooz@users.noreply.github.com>
2023-12-14 15:27:42 +0100

    Remove extra div from landingPageView

--- starwarswiki/src/views/landingPageView.jsx
+++ starwarswiki/src/views/landingPageView.jsx
@@ -1,4 +1,4 @@
-import '/src/style.css';
+//import '/src/style.css';
 
 import LandingCard from '../components/LandingCard';
 
@@ -7,27 +7,21 @@ export default function LandingPageView(props) {
 		<div>
 			<h1>STAR WARS WIKI</h1>
 			<div className='landing-cards'>
-				<div className='child'>
-					<LandingCard
-						text='CHARACTERS'
-						image='https://lumiere-a.akamaihd.net/v1/images/Yoda-Retina_2a7ecc26.jpeg'
-						linkTo='/characters'
-					/>
-				</div>
-				<div className='child'>
-					<LandingCard
-						text='VEHICLES'
-						image='https://lumiere-a.akamaihd.net/v1/images/millennium-falcon-main-tlj-a_7cf89d3a.jpeg'
-						linkTo='/vehicles'
-					/>
-				</div>
-				<div className='child'>
-					<LandingCard
-						text='LOCATIONS'
-						image='https://lumiere-a.akamaihd.net/v1/images/databank_mustafar_01_169_5b470758.jpeg'
-						linkTo='/locations'
-					/>
-				</div>
+				<LandingCard
+					text='CHARACTERS'
+					image='https://lumiere-a.akamaihd.net/v1/images/Yoda-Retina_2a7ecc26.jpeg'
+					linkTo='/characters'
+				/>
+				<LandingCard
+					text='VEHICLES'
+					image='https://lumiere-a.akamaihd.net/v1/images/millennium-falcon-main-tlj-a_7cf89d3a.jpeg'
+					linkTo='/vehicles'
+				/>
+				<LandingCard
+					text='LOCATIONS'
+					image='https://lumiere-a.akamaihd.net/v1/images/databank_mustafar_01_169_5b470758.jpeg'
+					linkTo='/locations'
+				/>
 			</div>
 		</div>
 	);
