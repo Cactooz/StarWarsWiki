39b0c1c25efb5408a4f8e5d5b39a1560dee82183
williamhedenskog <94441813+pilsnerfrajz@users.noreply.github.com>
2023-12-18 20:57:46 +0100

    Move <h3> to presenter

--- starwarswiki/src/views/profileView.jsx
+++ starwarswiki/src/views/profileView.jsx
@@ -6,9 +6,6 @@ export default function ProfileView(props) {
 			<SignOutButton />
 			<h2>Hello There {props.currentUser.displayName}!</h2>
 			<img src={props.currentUser.photoURL} />
-			<div>
-				<h3>These Are Your Favorites!</h3>
-			</div>
 		</>
 	);
 }
