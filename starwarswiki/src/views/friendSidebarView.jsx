e0040a23d6cce292e0ff73c62fd3a63bd2fb956f
Rikard Löwbeer <lowbeer@kth.se>
2023-12-18 13:19:11 +0100

    Update friendSidebarView.jsx
    Some minor changes

--- starwarswiki/src/views/friendSidebarView.jsx
+++ starwarswiki/src/views/friendSidebarView.jsx
@@ -4,16 +4,16 @@ export default function FriendSidebarView(props) {
 	return (
 		<>
 			<button
-				onClick={props.shouldShowId === false ? props.showID : props.hideID}>{props.shouldShowId === false ? "Click to show your ID" : "Click to hide your ID"}</button>
-			<p>{props.shouldShowId === true ? props.yourID : ""}</p>
-			<h3>{props.friendRequest.length ? "Your Friend Requests" : ""}</h3>
+				onClick={props.shouldShowId === false ? props.showID : props.hideID} >{props.shouldShowId === false ? "Click to show your ID" : "Click to hide your ID"}</button >
+			<p >{props.shouldShowId === true ? props.yourID : ""}</p >
+			<h3 >{props.friendRequest.length ? "Your Friend Requests" : ""}</h3 >
 			{props.friendRequest.length ? props.friendRequest.map(showFriends) : ""}
-			<h3>These Are Your Friends!</h3>
-			<p>Add friends with friend id:
-				<input type={"text"} placeholder={"Enter Your Friends ID"} onKeyUp={props.addfriend}/>
+			<h3 >These Are Your Friends!</h3 >
+			<p >Add friends with friend id:
+				<input type={"text"} placeholder={"Enter Your Friends ID"} onKeyUp={props.addfriend} />
 				{props.customMessage ? props.customMessage : ""}
-			</p>
-			<h3>{props.sentRequests.length ? "Waiting for answer from:" : ""}</h3>
+			</p >
+			<h3 >{props.sentRequests.length ? "Waiting for answer from:" : ""}</h3 >
 			{props.sentRequests.length ? props.sentRequests.map(showRequests) : ""}
 			<>
 				{props.friends.length ? props.friends.map(showAllCB) : "Add friends to show them here!"}
@@ -26,10 +26,10 @@ export default function FriendSidebarView(props) {
 			props.cancelFriend(friend)
 		}
 
-		return (<div key={friend}>
+		return (<div key={friend} >
 			{friend}
-			<button onClick={cancelFriend}>Cancel</button>
-		</div>)
+			<button onClick={cancelFriend} >Cancel</button >
+		</div >)
 	}
 
 	function showFriends(friend) {
@@ -41,11 +41,11 @@ export default function FriendSidebarView(props) {
 			props.declineFriend(friend)
 		}
 
-		return (<div key={friend}>
+		return (<div key={friend} >
 			{friend}
-			<button onClick={acceptFriend}>Accept</button>
-			<button onClick={declineFriend}>Decline</button>
-		</div>)
+			<button onClick={acceptFriend} >Accept</button >
+			<button onClick={declineFriend} >Decline</button >
+		</div >)
 	}
 
 	function showAllCB(friend) {
@@ -55,12 +55,12 @@ export default function FriendSidebarView(props) {
 		}
 
 		return (
-			<div key={friend}>
-				<Link to={"/profile/" + friend} key={friend}>
-					{friend}
-				</Link>
-				<button onClick={removeFriend}>Remove Friend</button>
-			</div>
+			<div key={friend} >
+				<Link to={"/profile/" + friend} key={friend} >
+					{props.names[friend]}
+				</Link >
+				<button onClick={removeFriend} >Remove Friend</button >
+			</div >
 		)
 	}
 }
