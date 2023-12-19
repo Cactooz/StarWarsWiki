f55ee2ff85014bc378205ce460f8c1e1580cabeb
Rikard Löwbeer <118805329+LowbeerR@users.noreply.github.com>
2023-12-19 17:37:48 +0100

    Bug with fast fetching (#73)
    * Fix error with fetch
    
    No more error with fetch
    
    * Update browsePresenter.jsx
    
    update
    
    * Update model.js
    
    Now only updates the browseResult after all 3 pages are fetched
    
    * Update browsePresenter.jsx
    
    Move out eventlistener and Useeffect, add listener for suspense and check that more data is available before trying to fetch more.
    
    * Update browsePresenter.jsx
    
    remove spinner is site is cached so it does not interract with animation
    
    * Update browsePresenter.jsx
    
    smoother scroll

--- starwarswiki/src/presenters/browsePresenter.jsx
+++ starwarswiki/src/presenters/browsePresenter.jsx
@@ -2,6 +2,7 @@ import BrowseView from '../views/browseView.jsx';
 import Vortex from '../components/Vortex.jsx';
 import { observer } from 'mobx-react-lite';
 import { useLocation } from 'react-router-dom';
+import { useEffect } from "react";
 
 export default observer(function Browse(props) {
 	function doAddACB(card) {
@@ -12,34 +13,33 @@ export default observer(function Browse(props) {
 		props.model.removeFromFavorites(card);
 	}
 
-	function handleScroll() {
+	async function handleScroll() {
 		const { scrollTop, clientHeight, scrollHeight } = document.documentElement;
-		if (scrollTop + clientHeight >= scrollHeight - clientHeight / 2) {
-			addData();
+		if (scrollTop + clientHeight >= scrollHeight - clientHeight * 2) {
+			if (props.model.browseResult.info.next !== null) {
+				await addData();
+			}
 		}
 	}
 
 	async function addData() {
 		let site = window.location.pathname.split('/');
-		if (
-			props.model.isLoading ||
+		if (props.model.isLoading ||
 			(site[1] !== 'vehicles' && site[1] !== 'characters' && site[1] !== 'locations') ||
 			site[2] ||
 			!site[1]
 		)
 			return;
 		await props.model.addMoreData();
-		return <Vortex />;
 	}
 
+
 	function render(browseResult) {
-		const site = useLocation().pathname.replace('/', '');
-		if (props.model.currentBrowse === undefined || props.model.currentBrowse !== site) {
-			props.model.setBrowseResult(site);
+		if (props.model.suspense) {
 			return <Vortex />;
-		} else if (browseResult === null) return <p>Error While Loading. Please Try Again!</p>;
-		else if (browseResult) {
-			addEventListener('scroll', handleScroll);
+		} else if (browseResult === null) {
+			return <p >Error While Loading. Please Try Again!</p >;
+		} else if (browseResult.data) {
 			return (
 				<BrowseView
 					browseResult={browseResult.data}
@@ -53,5 +53,13 @@ export default observer(function Browse(props) {
 		}
 	}
 
+	addEventListener('scroll', handleScroll);
+	const site = useLocation().pathname.replace('/', '');
+	useEffect(() => {
+		if (props.model.currentBrowse === undefined || props.model.currentBrowse !== site) {
+			if (!props.model.isLoading)
+				props.model.setBrowseResult(site);
+		}
+	}, [site, props.model.isLoading]);
 	return render(props.model.browseResult);
 });
