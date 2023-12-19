5cdaac2e69ed9ebad0337dcdf0834760928c26c1
williamhedenskog <94441813+pilsnerfrajz@users.noreply.github.com>
2023-12-19 14:25:46 +0100

    Move some HTML to view

--- starwarswiki/src/views/profileView.jsx
+++ starwarswiki/src/views/profileView.jsx
@@ -1,10 +1,23 @@
 import SignOutButton from '../components/SignOutButton.jsx';
+import { Link } from 'react-router-dom';
 
 export default function ProfileView(props) {
-	return (
-		<>
-			<SignOutButton />
-			<h2>Hello there, {props.name}!</h2>
-		</>
-	);
+	if (props.user === undefined) {
+		return (
+			<>
+				<h2>You are not logged in. Sign in above.</h2>
+				<Link to={'/'}>
+					<h2>Return to home!</h2>
+				</Link>
+			</>
+		);
+	} else {
+		return (
+			<>
+				<SignOutButton />
+				<h2>Hello there, {props.name}!</h2>
+				<h3>These are your favorite pages</h3>
+			</>
+		);
+	}
 }
