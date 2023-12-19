27d49fb4393eefcdc23263e5928d75f465b06f52
Rikard Löwbeer <lowbeer@kth.se>
2023-12-19 15:50:58 +0100

    Update browsePresenter.jsx
    Move out eventlistener and Useeffect, add listener for suspense and check that more data is available before trying to fetch more.

--- starwarswiki/src/presenters/browsePresenter.jsx
+++ starwarswiki/src/presenters/browsePresenter.jsx
@@ -13,40 +13,33 @@ export default observer(function Browse(props) {
 		props.model.removeFromFavorites(card);
 	}
 
-	function handleScroll() {
+	async function handleScroll() {
 		const { scrollTop, clientHeight, scrollHeight } = document.documentElement;
 		if (scrollTop + clientHeight >= scrollHeight - clientHeight / 2) {
-			addData();
+			if (props.model.browseResult.info.next !== null) {
+				await addData();
+			}
 		}
 	}
 
 	async function addData() {
 		let site = window.location.pathname.split('/');
-		if (
-			props.model.isLoading ||
+		if (props.model.isLoading ||
 			(site[1] !== 'vehicles' && site[1] !== 'characters' && site[1] !== 'locations') ||
 			site[2] ||
 			!site[1]
 		)
 			return;
 		await props.model.addMoreData();
-		return <Vortex />;
 	}
 
 
 	function render(browseResult) {
-		const site = useLocation().pathname.replace('/', '');
-		useEffect(() => {
-			if (props.model.currentBrowse === undefined || props.model.currentBrowse !== site) {
-				if (!props.model.isLoading)
-					props.model.setBrowseResult(site);
-			}
-		}, [site, props.model.isLoading]);
-		if (props.model.currentBrowse === undefined || props.model.currentBrowse !== site) {
+		if (props.model.currentBrowse === undefined || props.model.currentBrowse !== site || props.model.suspense) {
 			return <Vortex />;
-		} else if (browseResult === null) return <p >Error While Loading. Please Try Again!</p >;
-		else if (browseResult) {
-			addEventListener('scroll', handleScroll);
+		} else if (browseResult === null) {
+			return <p >Error While Loading. Please Try Again!</p >;
+		} else if (browseResult.data) {
 			return (
 				<BrowseView
 					browseResult={browseResult.data}
@@ -60,5 +53,13 @@ export default observer(function Browse(props) {
 		}
 	}
 
+	addEventListener('scroll', handleScroll);
+	const site = useLocation().pathname.replace('/', '');
+	useEffect(() => {
+		if (props.model.currentBrowse === undefined || props.model.currentBrowse !== site) {
+			if (!props.model.isLoading)
+				props.model.setBrowseResult(site);
+		}
+	}, [site, props.model.isLoading]);
 	return render(props.model.browseResult);
 });
