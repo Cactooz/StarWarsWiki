c5bb0b775efcfc1d5ef37658d4704b28a97add34
William Hedenskog <94441813+pilsnerfrajz@users.noreply.github.com>
2023-12-19 21:38:40 +0100

    Profile suspense (#77)
    * Reformat profilePresenter
    
    * Move <h3> to presenter
    
    * Remove profilePic, change "Hello there"
    
    * Remove unused props to view. Add suspense
    
    * Add check to see if favs are loading from DB
    
    * Add spinner while favorites are loading
    
    * Add p tag
    
    * Move some HTML to view
    
    * Fix profile view rendering

--- starwarswiki/src/presenters/profilePresenter.jsx
+++ starwarswiki/src/presenters/profilePresenter.jsx
@@ -1,36 +1,39 @@
-import {observer} from "mobx-react-lite";
-import ProfileView from "../views/profileView.jsx";
-import {Link} from "react-router-dom";
-import BrowseView from "../views/browseView.jsx";
+import { observer } from 'mobx-react-lite';
+import ProfileView from '../views/profileView.jsx';
+import BrowseView from '../views/browseView.jsx';
+import Vortex from '../components/Vortex.jsx';
 
-export default observer(
-    function ProfilePresenter(props) {
-        function doAddACB(card) {
-            props.model.addToFavorites(card)
-        }
+export default observer(function ProfilePresenter(props) {
+	function doAddACB(card) {
+		props.model.addToFavorites(card);
+	}
 
-        function doRemoveACB(card) {
-            props.model.removeFromFavorites(card)
-        }
-
-        if (props.model.user === undefined)
-            return (
-                <>
-                    <h2>You are not logged in. Sign in above.</h2>
-                    <Link to={"/"}>
-                        <h2>Return to home!</h2>
-                    </Link>
-                </>
-            )
-        if (props.model.user)
-            return (
-                <>
-                    <ProfileView currentUser={props.model.user} favorites={props.model.favorites}/>
-                    {props.model.favorites.length ?
-                        <BrowseView browseResult={props.model.favorites} doAdd={doAddACB} doRemove={doRemoveACB}
-                                    fav={props.model.favorites}
-                                    auth={props.model.user}/> : "You have not added any favorites yet..."}
-                </>
-            );
-    }
-);
+	function doRemoveACB(card) {
+		props.model.removeFromFavorites(card);
+	}
+	if (props.model.user === undefined) {
+		return <ProfileView user={props.model.user} />;
+	} else if (props.model.user)
+		return (
+			<>
+				{props.model.loadingFavs !== undefined && props.model.user.displayName ? (
+					<>
+						<ProfileView name={props.model.user.displayName} user={props.model.user} />
+						{props.model.favorites.length ? (
+							<BrowseView
+								browseResult={props.model.favorites}
+								doAdd={doAddACB}
+								doRemove={doRemoveACB}
+								fav={props.model.favorites}
+								auth={props.model.user}
+							/>
+						) : (
+							<p>You have not added any favorites yet...</p>
+						)}
+					</>
+				) : (
+					<Vortex />
+				)}
+			</>
+		);
+});
--- starwarswiki/src/views/profileView.jsx
+++ starwarswiki/src/views/profileView.jsx
@@ -1,14 +1,23 @@
 import SignOutButton from '../components/SignOutButton.jsx';
+import { Link } from 'react-router-dom';
 
 export default function ProfileView(props) {
-	return (
-		<>
-			<SignOutButton />
-			<h2>Hello There {props.currentUser.displayName}!</h2>
-			<img src={props.currentUser.photoURL} />
-			<div>
-				<h3>These Are Your Favorites!</h3>
-			</div>
-		</>
-	);
+	if (props.user === undefined) {
+		return (
+			<>
+				<h2>You are not logged in. Sign in above.</h2>
+				<Link to={'/'}>
+					<h2>Return to home!</h2>
+				</Link>
+			</>
+		);
+	} else {
+		return (
+			<>
+				<SignOutButton />
+				<h2>Hello there, {props.name}!</h2>
+				<h3>These are your favorite pages</h3>
+			</>
+		);
+	}
 }
