7a46915ab65e7e7f4306cc6d8e6dcc542ebc2173
Hugo <54435874+Cactooz@users.noreply.github.com>
2023-12-20 10:04:40 +0100

    Main styling (#84)
    * Remove duplicate css
    
    * Fix main margin-top based on header height
    
    * Update header css
    
    * Center main content and spinner
    
    * Move main margin top to App.jsx
    
    * Limit width of main to 3000 pixels
    
    * Fix default margin-top for main
    
    * Update main margin-top on page load
    
    * Fix browse cards being to big on smalls screens
    
    * Scale Vortex spinner on screen width

--- starwarswiki/src/App.jsx
+++ starwarswiki/src/App.jsx
@@ -4,6 +4,16 @@ import FooterPresenter from './presenters/footerPresenter.jsx';
 import AnimatedRoutes from './AnimatedRoutes.jsx';
 
 export default observer(function ReactRoot(props) {
+	window.onload = updateMainTopMargin;
+	window.onresize = updateMainTopMargin;
+
+	function updateMainTopMargin() {
+		const main = document.getElementById('main');
+		if (main) {
+			main.style.marginTop = `${document.getElementById('header')?.offsetHeight}px`;
+		}
+	}
+
 	return (
 		<>
 			<BrowserRouter>
