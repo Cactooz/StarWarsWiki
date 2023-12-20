8050392f7f623046400ccb461e6e0d22b012adc6
Hugo <54435874+Cactooz@users.noreply.github.com>
2023-12-20 23:32:55 +0100

    Add styling for burger menu

--- starwarswiki/src/views/hamburgerView.jsx
+++ starwarswiki/src/views/hamburgerView.jsx
@@ -2,6 +2,8 @@ import { slide as Menu } from 'react-burger-menu';
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
 
+import '../style/burgermenu.scss';
+
 export default function HamburgerView() {
 	return (
 		<Menu>
