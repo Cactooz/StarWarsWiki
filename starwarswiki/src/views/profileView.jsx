ec932ba94a23063027ac91d8c54741ab33fdb046
Pontus Berglund <60647163+pontaberglund@users.noreply.github.com>
2023-12-20 16:59:43 +0100

    Fix profile after rebase. Path and animation state to browse

--- starwarswiki/src/views/profileView.jsx
+++ starwarswiki/src/views/profileView.jsx
@@ -5,18 +5,26 @@ export default function ProfileView(props) {
 	if (props.user === undefined) {
 		return (
 			<>
-				<h2 >You are not logged in. Sign in above.</h2 >
-				<Link to={'/'} >
-					<h2 >Return to home!</h2 >
-				</Link >
+				<h2>You are not logged in. Sign in above.</h2>
+				<Link to={'/'} replace={props.inAnimation ? true : false}>
+					<h2>Return to home!</h2>
+				</Link>
 			</>
 		);
 	} else {
 		return (
 			<>
 				<SignOutButton />
-				<h2 >{props.user.displayName === undefined ? "Welcome to " + props.user + "'s Profile" : "Hello There " + props.user.displayName + "!"}</h2 >
-				<h3 >{props.user.displayName === undefined ? "These are " + props.user + "'s Favorites" : "These Are Your Favorites!"}</h3 >
+				<h2>
+					{props.user.displayName === undefined
+						? 'Welcome to ' + props.user + "'s Profile"
+						: 'Hello There ' + props.user.displayName + '!'}
+				</h2>
+				<h3>
+					{props.user.displayName === undefined
+						? 'These are ' + props.user + "'s Favorites"
+						: 'These Are Your Favorites!'}
+				</h3>
 			</>
 		);
 	}
