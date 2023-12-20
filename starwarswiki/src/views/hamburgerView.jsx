3d2b644b512894af3ee422a733a199f7cb8f371a
Pontus Berglund <60647163+pontaberglund@users.noreply.github.com>
2023-12-20 19:55:38 +0100

    Create HamburgerView

---
+++ starwarswiki/src/views/hamburgerView.jsx
@@ -0,0 +1,11 @@
+import { slide as Menu } from 'react-burger-menu';
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+
+export default function HamburgerView() {
+	return (
+		<Menu>
+			<p>Characters</p>
+		</Menu>
+	);
+}
