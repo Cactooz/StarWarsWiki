5042b12de1e56a61bc8f878984fb89c5dadfa0d9
williamhedenskog <94441813+pilsnerfrajz@users.noreply.github.com>
2023-12-20 17:27:56 +0100

    Add toast, change div to section
    Copy id to clipboard

--- starwarswiki/src/views/friendSidebarView.jsx
+++ starwarswiki/src/views/friendSidebarView.jsx
@@ -1,22 +1,35 @@
 import { Link } from 'react-router-dom';
+import Toastify from '../components/Toastify';
+import { toast } from 'react-toastify';
 
 export default function FriendSidebarView(props) {
 	return (
 		<>
-			<p>Add friend</p>
-			<input type={'text'} placeholder={`Enter your friend's ID`} onKeyUp={props.addfriend} />
-			<button onClick={props.shouldShowId === false ? props.showID : props.hideID}>
-				{props.shouldShowId === false ? 'Click to show your ID' : 'Click to hide your ID'}
-			</button>
-			{props.shouldShowId === true ? <p>props.yourID</p> : null}
-			{props.friendRequest.length ? <h3>Friend Requests</h3> : null}
-			{props.friendRequest.length ? <ul>{props.friendRequest.map(showFriends)}</ul> : null}
-			{props.sentRequests.length ? <h3>Pending Requests</h3> : null}
-			{props.sentRequests.length ? <ul>{props.sentRequests.map(showRequests)}</ul> : null}
-			{props.friends.length ? <h3>Your Friends</h3> : null}
-			{props.friends.length ? <ul>{props.friends.map(showAllCB)}</ul> : null}
+			<section>
+				<p>Add friend</p>
+				<input type={'text'} placeholder={`Enter your friend's ID`} onKeyUp={props.addfriend} />
+				<button onClick={showAndCopyID}>
+					{props.shouldShowId === false ? 'Click to show your ID' : 'Click to hide your ID'}
+				</button>
+				{props.shouldShowId === true ? <p>{props.yourID}</p> : null}
+				{props.friendRequest.length ? <h3>Friend Requests</h3> : null}
+				{props.friendRequest.length ? <ul>{props.friendRequest.map(showFriends)}</ul> : null}
+				{props.sentRequests.length ? <h3>Pending Requests</h3> : null}
+				{props.sentRequests.length ? <ul>{props.sentRequests.map(showRequests)}</ul> : null}
+				{props.friends.length ? <h3>Your Friends</h3> : null}
+				{props.friends.length ? <ul>{props.friends.map(showAllCB)}</ul> : null}
+			</section>
+			<Toastify />
 		</>
 	);
+
+	function showAndCopyID() {
+		props.shouldShowId === false ? props.showID() : props.hideID();
+		if (!props.shouldShowId) {
+			navigator.clipboard.writeText(props.yourID);
+			toast.info('Copied ID to clipboard!');
+		}
+	}
 
 	function showRequests(friend) {
 		function cancelFriend() {
