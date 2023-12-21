cb8ebd5bb0a4c52d9a274a765524012eead57429
Hugo <54435874+Cactooz@users.noreply.github.com>
2023-12-21 09:16:41 +0100

    Add alt text to all images (#99)

--- starwarswiki/src/views/landingPageView.jsx
+++ starwarswiki/src/views/landingPageView.jsx
@@ -11,18 +11,21 @@ export default function LandingPageView(props) {
 				<LandingCard
 					text='CHARACTERS'
 					image='https://lumiere-a.akamaihd.net/v1/images/Yoda-Retina_2a7ecc26.jpeg'
+					altText='Green Yoda in the swamp of Dagobah'
 					linkTo='/characters'
 					inAnimation={props.inAnimation}
 				/>
 				<LandingCard
 					text='VEHICLES'
 					image='https://lumiere-a.akamaihd.net/v1/images/millennium-falcon-main-tlj-a_7cf89d3a.jpeg'
+					altText='Millennium Falcon traveling in hyperspeed through space'
 					linkTo='/vehicles'
 					inAnimation={props.inAnimation}
 				/>
 				<LandingCard
 					text='LOCATIONS'
 					image='https://lumiere-a.akamaihd.net/v1/images/databank_mustafar_01_169_5b470758.jpeg'
+					altText='Dark mountainous and red lava filled volcanic landscape of the planet Mustafar'
 					linkTo='/locations'
 					inAnimation={props.inAnimation}
 				/>
