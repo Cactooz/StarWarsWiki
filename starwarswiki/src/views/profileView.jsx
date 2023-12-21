605ba06d3a808c247d14c61515cdc53997773298
Hugo <54435874+Cactooz@users.noreply.github.com>
2023-12-21 09:56:59 +0100

    Update profilPresenter layout & move h3 from View

--- starwarswiki/src/views/profileView.jsx
+++ starwarswiki/src/views/profileView.jsx
@@ -4,28 +4,23 @@ import { Link } from 'react-router-dom';
 export default function ProfileView(props) {
 	if (props.user === undefined) {
 		return (
-			<>
+			<section className='profile-title'>
 				<h2>You are not logged in. Sign in above.</h2>
 				<Link to={'/'} replace={props.inAnimation ? true : false}>
 					<h2>Return to home!</h2>
 				</Link>
-			</>
+			</section>
 		);
 	} else {
 		return (
-			<>
+			<section className='profile-title'>
 				<SignOutButton />
 				<h2>
 					{props.user.displayName === undefined
 						? 'Welcome to ' + props.user + "'s profile"
 						: 'Hello there, ' + props.user.displayName + '!'}
 				</h2>
-				<h3>
-					{props.user.displayName === undefined
-						? props.user + "'s favorite pages"
-						: 'Your favorite pages'}
-				</h3>
-			</>
+			</section>
 		);
 	}
 }
