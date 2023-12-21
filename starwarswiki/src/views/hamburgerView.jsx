d854e31519f6a944cb48e361942d172de22c7df3
Pontus Berglund <60647163+pontaberglund@users.noreply.github.com>
2023-12-21 10:03:27 +0100

    Add menu state and state management

--- starwarswiki/src/views/hamburgerView.jsx
+++ starwarswiki/src/views/hamburgerView.jsx
@@ -1,15 +1,35 @@
 import { slide as Menu } from 'react-burger-menu';
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
+import Logo from '../../assets/images/sww-logo.svg';
 
 import '../style/burgermenu.scss';
 
 export default function HamburgerView() {
+	const [menuOpen, setMenuOpen] = useState(false);
+
+	function handleOnClick() {
+		setMenuOpen(!menuOpen);
+	}
+
+	function handleOnOpen() {
+		setMenuOpen(true);
+	}
+
 	return (
-		<Menu>
-			<Link to='/characters'>Characters</Link>
-			<Link to='/vehicles'>Vehicles</Link>
-			<Link to='/locations'>Locations</Link>
+		<Menu isOpen={menuOpen} onOpen={handleOnOpen}>
+			<Link to='/' onClick={handleOnClick}>
+				<img src={Logo} alt='Star Wars Wiki logo of green Yoda with red Christmas hat' />
+			</Link>
+			<Link to='/characters' onClick={handleOnClick}>
+				Characters
+			</Link>
+			<Link to='/vehicles' onClick={handleOnClick}>
+				Vehicles
+			</Link>
+			<Link to='/locations' onClick={handleOnClick}>
+				Locations
+			</Link>
 		</Menu>
 	);
 }
