3b94824eb896079a858c090aa6d16124fdc5f5c5
Pontus <60647163+pontaberglund@users.noreply.github.com>
2023-12-21 20:00:38 +0100

    Scroll to top (#106)
    * Add scrollTo using useEffect
    
    * Add scrollTo on landing page
    
    * Add undefinded check in browse presenter
    
    * Add scroll to on profile page
    
    * Add scroll to top on search page

--- starwarswiki/src/presenters/landingPagePresenter.jsx
+++ starwarswiki/src/presenters/landingPagePresenter.jsx
@@ -1,6 +1,11 @@
 import LandingPageView from '../views/landingPageView';
 import { observer } from 'mobx-react-lite';
+import { useEffect } from 'react';
 
 export default observer(function LandingPagePresenter(props) {
+	useEffect(() => {
+		window.scrollTo(0, 0);
+	}, []);
+
 	return <LandingPageView inAnimation={props.model.inAnimation} />;
 });
