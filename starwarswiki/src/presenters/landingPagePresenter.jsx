5b6ecc26cc253a577b2e5aa76cdb8ec61e063858
Hugo <54435874+Cactooz@users.noreply.github.com>
2023-12-21 22:29:42 +0100

    Use EmblaCarousel on landingPage

--- starwarswiki/src/presenters/landingPagePresenter.jsx
+++ starwarswiki/src/presenters/landingPagePresenter.jsx
@@ -1,5 +1,7 @@
 import LandingPageView from '../views/landingPageView';
 import { observer } from 'mobx-react-lite';
+import { EmblaCarousel } from '../components/EmblaCarousel';
+import { getPreview } from '../models/firebaseModel';
 import { useEffect } from 'react';
 
 export default observer(function LandingPagePresenter(props) {
@@ -7,5 +9,30 @@ export default observer(function LandingPagePresenter(props) {
 		window.scrollTo(0, 0);
 	}, []);
 
-	return <LandingPageView inAnimation={props.model.inAnimation} />;
+	function getCarouselData() {
+		getPreview();
+		props.model.setCarouselLoading(true);
+	}
+
+	console.log('run');
+
+	const carouselData = Object.keys(props.model.carouselData).map((key) => {
+		return {
+			name: props.model.carouselData[key].name,
+			image: props.model.carouselData[key].image,
+			path: props.model.carouselData[key].path,
+		};
+	});
+
+	return (
+		<>
+			<LandingPageView inAnimation={props.model.inAnimation} />
+			<EmblaCarousel
+				data={carouselData}
+				loadingData={props.model.carouselLoading}
+				dataLoaded={props.model.carouselDataLoaded}
+				getData={getCarouselData}
+			/>
+		</>
+	);
 });
