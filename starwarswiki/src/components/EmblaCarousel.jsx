84461274397589c81a316fc1657681ce45ecfc72
Hugo <54435874+Cactooz@users.noreply.github.com>
2023-12-22 09:44:06 +0100

    Update embla buttons

--- starwarswiki/src/components/EmblaCarousel.jsx
+++ starwarswiki/src/components/EmblaCarousel.jsx
@@ -22,19 +22,23 @@ export function EmblaCarousel(props) {
 
 	return (
 		<div className='embla' ref={emblaRef}>
-			<div className='embla__container'>
+			<div className='embla-container'>
 				{props.data.map((item) => (
-					<div className='embla__slide' key={item.name}>
+					<div className='embla-slide' key={item.name}>
 						<p>{item.name}</p>
 						<img src={item.image} />
 					</div>
 				))}
 			</div>
-			<button className='embla__prev' onClick={scrollPrev}>
-				Prev
+			<button className='embla-prev' onClick={scrollPrev}>
+				<svg xmlns='http://www.w3.org/2000/svg' height='16' width='10' viewBox='0 0 320 512'>
+					<path d='M41.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.3 256 246.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z' />
+				</svg>
 			</button>
-			<button className='embla__next' onClick={scrollNext}>
-				Next
+			<button className='embla-next' onClick={scrollNext}>
+				<svg xmlns='http://www.w3.org/2000/svg' height='16' width='10' viewBox='0 0 320 512'>
+					<path d='M278.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-160 160c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L210.7 256 73.4 118.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l160 160z' />
+				</svg>
 			</button>
 		</div>
 	);
