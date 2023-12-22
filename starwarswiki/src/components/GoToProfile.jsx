3d3e6eb99de73722753478063f342ae84c23fa19
Rikard Löwbeer <lowbeer@kth.se>
2023-12-22 10:24:24 +0100

    Update GoToProfile.jsx
    add condition for link to profile only if not on profile

--- starwarswiki/src/components/GoToProfile.jsx
+++ starwarswiki/src/components/GoToProfile.jsx
@@ -1,13 +1,16 @@
 import { Link } from 'react-router-dom';
 
 export default function GoToProfile(props) {
-	return (
-		<Link
-			to={'/profile'}
-			replace={props.inAnimation ? true : false}
-			onClick={props.onClickHandler ? props.onClickHandler : () => {}}
-		>
-			<p>Profile</p>
-		</Link>
-	);
+	if (props.page === '/profile')
+		return <p className='currentPage'>Profile</p>;
+	else
+		return (
+			<Link
+				to={'/profile'}
+				replace={props.inAnimation ? true : false}
+				onClick={props.onClickHandler ? props.onClickHandler : () => {
+				}}>
+				<p>Profile</p>
+			</Link>
+		);
 }
