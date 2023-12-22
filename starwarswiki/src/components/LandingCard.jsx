5e01b86590e429068f384f0e12eb2f2c14b199e3
Hugo <54435874+Cactooz@users.noreply.github.com>
2023-12-22 18:04:26 +0100

    Set landingCard to section

--- starwarswiki/src/components/LandingCard.jsx
+++ starwarswiki/src/components/LandingCard.jsx
@@ -2,11 +2,11 @@ import { Link } from 'react-router-dom';
 
 export default function LandingCard(props) {
 	return (
-		<div>
+		<section>
 			<Link to={props.linkTo} replace={props.inAnimation ? true : false}>
 				<img src={props.image} alt={props.altText} />
 				<p>{props.text}</p>
 			</Link>
-		</div>
+		</section>
 	);
 }
