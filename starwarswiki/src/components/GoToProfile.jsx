02d6de4154b6bf9d2043ac2d9831c258833df970
Hugo <54435874+Cactooz@users.noreply.github.com>
2023-12-22 16:23:54 +0100

    Fix styling (#119)
    * Fix animation-div not wrapping
    
    * Fix main margin top not updating on page change
    
    * Fix all text to have default cursor
    
    * Update navbar and heamburger to only have a tag
    
    * Remove p from GoToProfile
    
    * Fix cards cursor pointer on hover over text
    
    * Fix details and landing styling and layout
    
    * Fix extra padding shown for all section h2
    
    * Fix background bounce on Chrome/Mac
    
    ---------
    
    Co-authored-by: williamhedenskog <94441813+pilsnerfrajz@users.noreply.github.com>

--- starwarswiki/src/components/GoToProfile.jsx
+++ starwarswiki/src/components/GoToProfile.jsx
@@ -1,16 +1,15 @@
 import { Link } from 'react-router-dom';
 
 export default function GoToProfile(props) {
-	if (props.page === '/profile')
-		return <p className='active'>Profile</p>;
+	if (props.page === '/profile') return <p className='active'>Profile</p>;
 	else
 		return (
 			<Link
 				to={'/profile'}
 				replace={props.inAnimation ? true : false}
-				onClick={props.onClickHandler ? props.onClickHandler : () => {
-				}}>
-				<p>Profile</p>
+				onClick={props.onClickHandler ? props.onClickHandler : () => {}}
+			>
+				Profile
 			</Link>
 		);
 }
