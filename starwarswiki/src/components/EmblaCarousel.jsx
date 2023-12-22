790aea9b9d832ce587f11dd0aca887b0aa601dcb
Hugo <54435874+Cactooz@users.noreply.github.com>
2023-12-22 12:10:13 +0100

    Add embla carousel at landing page (#116)
    * Update firebaseModel.js
    
    getting 9 random info from starwarsdatabank
    
    * Update model.js
    
    new model property for storing 9 random cards
    
    * Update getPreview to only return name, image, path
    
    * Update model with Carousel items
    
    * Install emble carousel
    
    * Install embla-carousel-autoplay
    
    * Add autospin EmblaCarousel with next/prev buttons
    
    * Map data from firebase in EmblaCarousel
    
    * Use EmblaCarousel on landingPage
    
    * Remove missed console.log
    
    * Fix base styling for emblaCarousel
    
    * Fix all emble slides having the same key
    
    * Stop duplicates in getPreview for carouselle
    
    * Fix embla styling
    
    * Fix mobile view for embla
    
    * Update embla buttons
    
    * Use LandingCards in embla carousel
    
    * Remove unused className from LandingCard
    
    * Add title to embla carousel
    
    * Fix landing-page cards to not wrap
    
    * Fix image sizes in embla
    
    * Fix embla item hover & background loading effect
    
    * Fix landing page styling
    
    * Fix typo on landingPage
    
    * Remove redundant alt text from carousel
    
    * Larger yellow text stroke
    
    * Remove main-title margin-bottom
    
    ---------
    
    Co-authored-by: Rikard Löwbeer <lowbeer@kth.se>

---
+++ starwarswiki/src/components/EmblaCarousel.jsx
@@ -0,0 +1,52 @@
+import React, { useCallback } from 'react';
+import useEmblaCarousel from 'embla-carousel-react';
+import Autoplay from 'embla-carousel-autoplay';
+import Vortex from './Vortex';
+import LandingCard from './LandingCard';
+
+import '../style/embla.scss';
+
+export function EmblaCarousel(props) {
+	const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true }, [Autoplay()]);
+
+	const scrollPrev = useCallback(() => {
+		if (emblaApi) emblaApi.scrollPrev();
+	}, [emblaApi]);
+
+	const scrollNext = useCallback(() => {
+		if (emblaApi) emblaApi.scrollNext();
+	}, [emblaApi]);
+
+	if (!props.dataLoaded) {
+		if (!props.loadingData) props.getData();
+		return <Vortex />;
+	}
+
+	return (
+		<div className='embla' ref={emblaRef}>
+			<div className='embla-container'>
+				{props.data.map((item) => (
+					<div className='embla-slide' key={item.name}>
+						<LandingCard
+							text={item.name}
+							image={item.image}
+							altText=''
+							linkTo={`${item.path}/${item.name.replaceAll('/', '%2F').replaceAll('.', '%2E')}`}
+							inAnimation={props.inAnimation}
+						/>
+					</div>
+				))}
+			</div>
+			<button className='embla-prev' onClick={scrollPrev}>
+				<svg xmlns='http://www.w3.org/2000/svg' height='16' width='10' viewBox='0 0 320 512'>
+					<path d='M41.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.3 256 246.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z' />
+				</svg>
+			</button>
+			<button className='embla-next' onClick={scrollNext}>
+				<svg xmlns='http://www.w3.org/2000/svg' height='16' width='10' viewBox='0 0 320 512'>
+					<path d='M278.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-160 160c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L210.7 256 73.4 118.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l160 160z' />
+				</svg>
+			</button>
+		</div>
+	);
+}
--- starwarswiki/src/components/LandingCard.jsx
+++ starwarswiki/src/components/LandingCard.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 export default function LandingCard(props) {
 	return (
-		<div className='loading-card'>
+		<div>
 			<Link to={props.linkTo} replace={props.inAnimation ? true : false}>
 				<img src={props.image} alt={props.altText} />
 				<p>{props.text}</p>
